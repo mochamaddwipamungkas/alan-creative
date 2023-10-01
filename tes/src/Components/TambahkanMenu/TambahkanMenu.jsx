8e67a191f2e0d38f43b42fb38a890a93c4e89ce3
mochamaddwipamungkas <mdpamungkas15@gmail.com>
2023-10-01 20:16:50 +0700

    upload2

--- tes/src/Components/TambahkanMenu/TambahkanMenu.jsx
+++ tes/src/Components/TambahkanMenu/TambahkanMenu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./TambahkanMenu.css";
+import Dropzone from "react-dropzone";
 
 import Navbar from "../Navbar/Navbar";
 
@@ -7,19 +8,31 @@ const TambahkanMenu = () => {
   return (
     <div className="containerPage">
       <Navbar />
+
       <div className="tambahkanMenu mt-5">
         <div className="p-5 shadow" style={{ backgroundColor: "white" }}>
           <p className="fs-5 fw-bold formTitle">Tambahkan Menu</p>
+
           <form action="">
             <label htmlFor="name">Nama Menu</label>
             <input type="text" className="form-control mt-2" />
             <label htmlFor="foto" className="mt-4">
               Foto Menu
             </label>
-            <div className="formFotoMenu text-center mt-2">
-              <img src={require("../../assets/upload.png")} alt="" />
-              <p>drag and drop a file here or click</p>
-            </div>
+
+            <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+              {({ getRootProps, getInputProps }) => (
+                <section>
+                  <div {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    <div className="formFotoMenu text-center mt-2">
+                      <img src={require("../../assets/upload.png")} alt="" />
+                      <p>drag and drop a file here or click</p>
+                    </div>
+                  </div>
+                </section>
+              )}
+            </Dropzone>
             <label htmlFor="name" className="mt-4">
               Harga Menu
             </label>
--- tes/src/Pages/TransaksiPage/TransaksiPage.jsx
+++ tes/src/Pages/TransaksiPage/TransaksiPage.jsx
@@ -1,34 +1,51 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { memo, useCallback, useEffect, useState } from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import Invoice from "../../Components/Invoice/Invoice";
-import CardProduct from "../../Components/CardProduct/CardProduct";
 import "./TransaksiPage.css";
+import "../../Components/CardProduct/CardProduct.css";
 import getProducts from "../../services/product.service";
 
 const TransaksiPage = () => {
   const [products, setProducts] = useState([]);
-  const [bill, setBill] = useState([
-    {
-      id: 2,
-      qty: 1,
-    },
-  ]);
+  const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
     getProducts((data) => {
       setProducts(data);
+      console.log(data);
+      setCart(JSON.parse(localStorage.getItem("cart")) || []);
     });
   }, []);
 
-  const handleAddProduct = useCallback((id) => {
-    setBill([
-      ...bill,
-      {
-        id,
-        qty: 1,
-      },
-    ]);
-  });
+  useEffect(() => {
+    if (cart.length > 0) {
+      const sum = cart.reduce((acc, item) => {
+        const product = products.find((product) => product.id === item.id);
+        return acc + product.price * item.qty;
+      }, 0);
+      setTotalPrice(sum);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart]);
+
+  const handleAddProduct = (id) => {
+    if (cart.find((item) => item.id === id)) {
+      setCart(
+        cart.map((item) =>
+          item.id === id ? { ...item, qty: item.qty + 1 } : item
+        )
+      );
+    } else {
+      setCart([...cart, { id, qty: 1 }]);
+    }
+  };
+
+  const handleClearCart = () => {
+    localStorage.removeItem("cart");
+    setCart([]);
+    setTotalPrice(0);
+  };
 
   return (
     <div className="containerPage">
@@ -38,19 +55,127 @@ const TransaksiPage = () => {
           <div className="listProduks">
             {products.length > 0 &&
               products.map((product) => (
-                <CardProduct
+                <div
                   key={product.id}
-                  id={product.id}
-                  name={product.name}
-                  price={product.price}
-                  image={product.image_url}
-                  handleAddProduct={handleAddProduct}
-                />
+                  className="card"
+                  onClick={() => handleAddProduct(product.id)}
+                >
+                  <div className="cardImg">
+                    <img src={product.image_url} alt="" />
+                  </div>
+                  <div className="cardText">
+                    <p className="text-dark">{product.name}</p>
+                    <p>Rp {product.price.toLocaleString()}</p>
+                  </div>
+                </div>
+                // <CardProduct
+                //   key={product.id}
+                //   id={product.id}
+                //   name={product.name}
+                //   price={product.price}
+                //   image={product.image_url}
+                //   handleAddProduct={handleAddProduct}
+                // />
               ))}
           </div>
         </div>
         <div>
-          <Invoice bill={bill}>{products}</Invoice>
+          <div className="invoice">
+            <div className="invoiceItem">
+              <div className="invoiceHeader">
+                <img src={require("../../assets/account.png")} alt="" />
+                <p>Pesanan</p>
+              </div>
+              {cart.map((item, index) => {
+                const product = products.find(
+                  (product) => product.id === item.id
+                );
+                console.log(product);
+                return (
+                  <div key={index} className="item">
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td>
+                            <img src={product.image_url} alt="" />
+                          </td>
+                          <td>{product.name}</td>
+                          <td>x {item.qty}</td>
+                          <td style={{ color: "#28b0ff" }}>
+                            Rp. {product.price.toLocaleString()}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="invoiceButton">
+              <div style={{ display: "flex" }}>
+                <button
+                  className="btn btn-cart "
+                  onClick={() => handleClearCart()}
+                >
+                  Clear Cart
+                </button>
+              </div>
+              <div style={{ display: "flex" }}>
+                <button
+                  href=""
+                  className="btn btn-saveBill"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                >
+                  Save Bill
+                </button>
+                {/* <!-- Modal --> */}
+                <div
+                  className="modal fade"
+                  id="exampleModal"
+                  aria-labelledby="exampleModalLabel"
+                  aria-hidden="true"
+                >
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <button
+                          type="button"
+                          className="btn-close"
+                          data-bs-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      <div className="modal-body">
+                        Apakah anda yakin untuk save bill ini ?
+                      </div>
+                      <div className="modal-footer">
+                        <button
+                          type="button"
+                          className="btn btn-secondary"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                        <button type="button" className="btn btn-primary">
+                          Save changes
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <button href="" className="btn btn-saveBill">
+                  Print Bill
+                </button>
+              </div>
+              <div style={{ display: "flex" }}>
+                <button href="" className="btn btn-charge">
+                  Rp. {totalPrice.toLocaleString()}
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* <Invoice cart={cart}>{products}</Invoice> */}
         </div>
       </section>
     </div>
